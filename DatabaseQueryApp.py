da8657bdd060b89a7b7eb86d0d36bdf9953bb7c3
sadiq638 <108665432+sadiq638@users.noreply.github.com>
2022-07-05 22:36:13 +0530

    Add files via upload

---
+++ DatabaseQueryApp.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+def show_all():
+	""" this function fetches all records in a table"""
+	connection=sqlite3.connect('customer.db')
+	c=connection.cursor()
+	c.execute('SELECT rowid, * FROM customer')
+	records=c.fetchall()
+	for items in records:
+		print(items)
+	connection.commit()
+	connection.close()
+
+def add_one_record(first,last,email):
+	""" this function add one record to the table"""
+	connection=sqlite3.connect('customer.db')
+	c=connection.cursor()
+	c.execute("INSERT INTO customer VALUES(?,?,?)", (first,last,email))
+	connection.commit()
+	connection.close()
+def add_many_record(list):
+	""" this function adds many records to the table"""
+	connection=sqlite3.connect('customer.db')
+	c=connection.cursor()
+	c.executemany("INSERT INTO customer VALUES(?,?,?)", list)
+	connection.commit()
+	connection.close()
+def email_lookup(email):
+	""" this function fetches all records in a table baed on certain condtions (email id)"""
+	connection=sqlite3.connect('customer.db')
+	c=connection.cursor()
+	c.execute("SELECT rowid, * FROM customer WHERE email_id = (?)", (email,))
+	records=c.fetchall()
+	for items in records:
+		print(items)
+	connection.commit()
+	connection.close()
+email_lookup('ramos@outlook.com')
+
+
